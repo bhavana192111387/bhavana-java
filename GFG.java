357cf30cc284f09632b0cb440280c1fd33ce3df6
bhavana192111387 <112713393+bhavana192111387@users.noreply.github.com>
2022-09-13 10:01:04 +0530

    Add files via upload

--- GFG.java
+++ GFG.java
@@ -1,29 +1,14 @@
-//Java implementation of the approach
-import java.io.*;
+
+class GFG {
  
-class GFG
-{
-     
-// Function to print all the perfect
-// squares from the given range
-static void perfectSquares(int l, int r)
-{
- 
-    // For every element from the range
-    for (int i = l; i <= r; i++)
+    // Main driver method
+    public static void main(String[] args)
     {
- 
-        // If current element is
-        // a perfect square
-        if (Math.sqrt(i) == (int)Math.sqrt(i))
-            System.out.print(i + " ");
+         // Custom integer input
+        int i = 97;
+         // Type casting character to integer
+        char ch = (char)i;
+        // Print statement
+        System.out.println(ch);
     }
-}
- 
-// Driver code
-public static void main (String[] args)
-{
-    int l = 2, r = 24;
-    perfectSquares(l, r);
-}
 }
